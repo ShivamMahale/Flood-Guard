aec8c4cb63eb69a9964d232dee62a80b918cf134
bachannayak <bachan.nayak2018@gift.edu.in>
2024-06-16 13:44:46 +0530

    db file updated

--- db.py
+++ db.py
@@ -38,25 +38,6 @@ def insert_user(email, username, password):
         cursor.close()
         conn.close()
 
-def fetch_users_emails():
-    """Fetch user emails and return them as a list."""
-    conn = get_db_connection()
-    cursor = conn.cursor()
-    cursor.execute("SELECT email FROM users")
-    emails = [row[0] for row in cursor.fetchall()]
-    cursor.close()
-    conn.close()
-    return emails
-
-def fetch_usernames():
-    """Fetch all usernames in the database and return them as a list."""
-    conn = get_db_connection()
-    cursor = conn.cursor()
-    cursor.execute("SELECT username FROM users")
-    usernames = [row[0] for row in cursor.fetchall()]
-    cursor.close()
-    conn.close()
-    return usernames
 
 def get_user(email):
     """Return a particular user if exists, else None."""
@@ -95,69 +76,4 @@ def hash_password(password):
 def verify_password(stored_password, provided_password):
     """Verify a stored password against one provided by user."""
     return stored_password == hashlib.sha256(provided_password.encode()).hexdigest()
-
-def sign_up():
-    with st.form(key='signup', clear_on_submit=False):
-        st.subheader('Sign up')
-        email = st.text_input('Email', placeholder='Enter email')
-        username = st.text_input('Username', placeholder='Enter Username')
-        password = st.text_input('Password', placeholder='Enter password', type='password')
-        password2 = st.text_input('Password', placeholder='Confirm password', type='password')
-
-        if email:
-            if validate_email(email):
-                if email not in fetch_users_emails():
-                    if validate_username(username):
-                        if len(username) >= 2:
-                            if username not in fetch_usernames():
-                                if len(password) >= 6:
-                                    if password == password2:
-                                        hashed_password = hash_password(password)
-                                        insert_user(email=email, username=username, password=hashed_password)
-                                        st.success('Account created Successfully')
-                                        st.balloons()
-                                    else:
-                                        st.warning('Passwords do not match')
-                                else:
-                                    st.warning('Password should be at least 6 characters')
-                            else:
-                                st.warning('Username Already Exists')
-                        else:
-                            st.warning('Username too short')
-                    else:
-                        st.warning('Invalid characters in Username')
-                else:
-                    st.warning('Email Already Exists!!')
-            else:
-                st.warning('Invalid email Address')
-
-        col1, col2, col3, col4, col5 = st.columns(5)
-        with col3:
-            st.form_submit_button('Sign up')
-
-def login():
-    with st.form(key='login', clear_on_submit=False):
-        st.subheader('Login')
-        email = st.text_input('Email', placeholder='Enter email')
-        password = st.text_input('Password', placeholder='Enter password', type='password')
-        if email:
-            if validate_email(email):
-                if email in fetch_users_emails():
-                    user = get_user(email)
-                    if user and verify_password(user['password'], password):
-                        st.session_state.login_success = True
-                        st.session_state.user_name = user['username']
-                        st.success('Logged in successfully!')
-                    else:
-                        st.warning('Invalid email or password')
-                else:
-                    st.warning('Email Does Not Exist. Please Sign up')
-            else:
-                st.warning('Invalid Email Address')
-
-        col1, col2, col3, col4, col5 = st.columns(5)
-        with col3:
-            st.form_submit_button('Login')
-
-
-
+    
